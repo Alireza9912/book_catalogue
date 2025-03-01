29b3d86b2eee6e9f50d0a98c516a404eaa5efa7f
Alireza enayti <alirezaenayti@gmail.com>
2025-03-01 02:57:04 -0800

    make superuser

--- catalogue/books/views.py
+++ catalogue/books/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
 def home(request):
+    Books = Book.objects.all()
     return render(request, 'book_list.html')
+
+
 
 # Create your views here.
